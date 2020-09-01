80fe3614784620845ce1769cf8abe95423123585
Carolene Wilson <carolenesw@gmail.com>
2020-08-31 20:20:28 -0400

    create route for heroku

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
+// require npm packages 
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+// create route for user to have access to app
+require('dotenv').config()
+
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
 app.use(logger("dev"));
 
 app.use(compression());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+app.set('port', PORT);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+ useNewUrlParser: true,
+ useFindAndModify: false,
+ useUnifiedTopology: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
-
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+ console.log(`App running on port ${PORT}!`);
 });
+
+
