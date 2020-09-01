767599785b9129960f14691e34b5caf02aba79a0
Carolene Wilson <carolenesw@gmail.com>
2020-08-31 20:22:56 -0400

    create service worker file for connection

---
+++ public/serviceworker.js
@@ -0,0 +1,86 @@
+// require files 
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/images/ledger.jpg",
+];
+
+const CACHE_MONEY = "static-cache-original";
+const DATA_MONEY_NEW = "data-cache-new";
+// set up install
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches
+      .open(CACHE_MONEY)
+      .then((cache) => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+      .catch(function (err) {
+        console.log("not running", err);
+      })
+  );
+  self.skipWaiting();
+});
+// activate
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_MONEY && key !== DATA_MONEY_NEW) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// fetch data 
+self.addEventListener("fetch", (event) => {
+  // non GET requests are not cached and requests to other origins are not cached
+  if (
+    event.request.method !== "GET" ||
+    !event.request.url.startsWith(self.location.origin)
+  ) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+  // handle runtime GET requests for data from /api routes
+  if (event.request.url.includes("/api/")) {
+    // make network request and fallback to cache if network request fails (offline)
+    event.respondWith(
+      caches.open(DATA_MONEY_NEW).then((cache) => {
+        return fetch(event.request)
+          .then((response) => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => caches.match(event.request));
+      })
+    );
+    return;
+  }
+  // use cache first for all other requests for performance
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      // request is not in cache. make network request and cache the response
+      return caches.open(DATA_MONEY_NEW).then((cache) => {
+        return fetch(event.request).then((response) => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
+          });
+        });
+      });
+    })
+  );
+});
